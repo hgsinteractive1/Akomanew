973608c99bafad13407396d8bd31560964a9a3bf
Robert Hanley <roberthanley@Roberts-iMac.local>
2015-05-02 01:20:05 -0400

    Continued work on core template structure. Focus on category and story detail pages.

---
+++ static/includes/category_hero.php
@@ -0,0 +1,12 @@
+<!--BEGIN CATEGORY HERO AREA-->
+<div id="category-hero">
+	<div id="image-area">
+		<img src="images/default_hero.jpg" />
+		<div id="tint"></div>
+	</div>
+	<div id="headline">
+		<h3>Hits<span class="circle-decoration"></span><span class="cat-descrip">Lorem ipsum dolor</span></h3>
+		<h4>A look into a hashtag's unintended consequences in Nigeria</h4>
+	</div>
+</div>
+<!--END CATEGORY HERO AREA-->
---
+++ static/story_detail.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+
+<title>Home Template</title>
+
+
+</head>
+<body class="detail">
+
+<!--BEGIN STORY DETAIL -->
+<div id="story-intro">
+	<h2>Can the in-coming government fix Nigeria's economy?</h2>
+	<div class="author">
+		<div class="author-image"></div>
+		<div class="byline">
+			<p>Timothy Johnson <span>2 hours ago</span></p>
+
+		</div>
+	</div>
+		<div class="recommend">
+			<div class="heart"></div>
+			<div class="count">23</div>
+		</div>
+		<div class="clear"></div>
+</div>
+<!--END STORY DETAIL-->
+
+<!--MAIN NAVIGATION-->
+<?php include('includes/footer.php') ?>
+<!--END-->
+
+<!--MAIN NAVIGATION-->
+<?php include('includes/nav_bar.php') ?>
+<!--END-->
+
+<!--INCLUDE REQUIRE/INIT MAIN SCRIPTS-->
+<script src="js/require.js" data-main="js/core"></script>
+<!--END-->
+</body>
+</html>
