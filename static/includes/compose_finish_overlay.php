e112a1d52cc645968167fe750b6c8d63a683cc64
Robert Hanley <roberthanley@Roberts-iMac.local>
2015-05-07 00:26:03 -0400

    Finished basic structure for tag overlay. Need to fix responsive states and image icons

---
+++ static/includes/compose_finish_overlay.php
@@ -0,0 +1,41 @@
+<div id="finish-overlay">
+	<div class="relative">
+		<div id="content">
+			
+			<!--TOP DIRECTIONS-->
+			<div id="top-directions">
+				<h4>Done Editing?</h4>
+				<p>Change your story’s title, subtitle and add relevant tags.</p>
+			</div>
+			<!--END TOP DIRECTIONS-->
+			<!--BEGIN SUMMARY DETAILS-->
+			<div id="summary-details">
+				<div class="story-info">
+					<h4>Can the in-coming government fix Nigeria’s economy?</h4>
+					<div class="summary">
+						<div contenteditable="true"><p>Nigeria’s 2015 presidential election may have been won and lost but while still savouring his victory at the poll.</p></div>
+						<div class="edit-icon"></div>
+					</div>
+					<ul class="tag-list">
+						<li class="label"><h4>Tag your story:</h4></li>
+						<li class="tag"><a href="#">Test</a></li>
+						<li class="tag"><a href="#" class="active">Test</a></li>
+						<li class="tag"><a href="#">Test</a></li>
+						<li class="tag"><a href="#">Test</a></li>
+					</ul>
+					<p class="tag-descrip">Stories may have multiple tags</p>
+				</div>
+			</div>
+			<!--END SUMMARY DETAILS-->
+			<!--BEGIN ACTION AREA/BUTTONS -->
+			<div id="action-area">
+				<ul class="action-list">
+					<li><a href="#" class="action-button continue-editing">Continue Editing</a></li>
+					<li><a href="#" class="action-button save-to-drafts">Save to Drafts</a></li>
+					<li class="last"><a href="#" class="action-button publish">Publish</a></li>
+				</ul>
+			</div>
+			<!--END ACTION AREA-->
+		</div>
+	</div>
+</div>
