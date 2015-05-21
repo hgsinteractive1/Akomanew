4c91933cc8397d0f6e803f310da0b25cbb7ecb58
Robert Hanley <roberthanley@Roberts-iMac.local>
2015-05-21 01:06:13 -0400

    Completed HTML/CSS for stories and drafts page

---
+++ static/stories_and_drafts.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+
+<title>Home Template</title>
+
+
+</head>
+<body class="detail">
+
+<!--BEGIN STORIES AND DRAFTS -->
+<div id="stories-and-drafts">
+	<!--BEGIN TOGGLE-->
+	<ul id="toggle">
+		<li class="left"><a href="#" class="toggle-link active">Drafts <span>3</span></a></li>
+		<li><a href="#" class="toggle-link">Published <span>20</span></a></li>
+	</ul>
+	<!--END TOGGLE-->
+	<!--BEGIN STORIES LIST-->
+	<ul id="stories-list">
+		<li class="story-item">
+			<h3>Thousands March in Memory of Those Lost to Boko Haram</h3>
+			<p>In Nigeria's capital of Abuja, thousands of women mark the first anniversary of the abduction of more than 219 schoolgirls by several Boko Haram militants.</p>
+			<span class="last-edited">Last edited 3 days ago</span>
+			<ul class="stories-actions">
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						<span class="remove-mobile-text">Continue </span>Edit<span class="remove-mobile-text">ing</span>
+						<div class="continue-editing action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						Delete
+						<div class="delete-draft action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button publish">
+						Publish
+						<div class="publish-draft action-icon"></div>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li class="story-item">
+			<h3>Thousands March in Memory of Those Lost to Boko Haram</h3>
+			<p>In Nigeria's capital of Abuja, thousands of women mark the first anniversary of the abduction of more than 219 schoolgirls by several Boko Haram militants.</p>
+			<span class="last-edited">Last edited 3 days ago</span>
+			<ul class="stories-actions">
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						<span class="remove-mobile-text">Continue </span>Edit<span class="remove-mobile-text">ing</span>
+						<div class="continue-editing action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						Delete
+						<div class="delete-draft action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button publish">
+						Publish
+						<div class="publish-draft action-icon"></div>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li class="story-item">
+			<h3>Thousands March in Memory of Those Lost to Boko Haram</h3>
+			<p>In Nigeria's capital of Abuja, thousands of women mark the first anniversary of the abduction of more than 219 schoolgirls by several Boko Haram militants.</p>
+			<span class="last-edited">Last edited 3 days ago</span>
+			<ul class="stories-actions">
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						<span class="remove-mobile-text">Continue </span>Edit<span class="remove-mobile-text">ing</span>
+						<div class="continue-editing action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button">
+						Delete
+						<div class="delete-draft action-icon"></div>
+					</a>
+				</li>
+				<li class="story-action">
+					<a href="#" class="story-action-button publish">
+						Publish
+						<div class="publish-draft action-icon"></div>
+					</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<!--END STRIES LIST-->
+
+</div>
+<!--END STORIES AND DRAFTS -->
+
+
+<!--MAIN NAVIGATION-->
+<?php include('includes/nav_bar.php') ?>
+<!--END-->
+
+<!--MAIN MENU OVERLAY-->
+<?php include('includes/main_menu.php') ?>
+<!--END-->
+
+<!--MAIN MENU ICON-->
+<a href="#" id="menu-icon">
+	<div id="relative">
+		<div class="menu-bar one"></div>
+		<div class="menu-bar two"></div>
+		<div class="menu-bar three"></div>
+	</div>
+</a>
+<!--END MENU-->
+
+<!--MAIN NAVIGATION-->
+<?php include('includes/compose_finish_overlay.php') ?>
+<!--END-->
+
+<!--INCLUDE REQUIRE/INIT MAIN SCRIPTS-->
+<script src="js/require.js" data-main="js/core"></script>
+<!--END-->
+</body>
+</html>
