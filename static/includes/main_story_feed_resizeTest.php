d18a0b718a6e89abec405b20aa63b74c5f1bd15c
Robert Hanley <roberthanley@Roberts-iMac.local>
2015-06-03 19:51:30 -0400

    Added templates for resizing

---
+++ static/includes/main_story_feed_resizeTest.php
@@ -0,0 +1,140 @@
+<div id="main-story-feed">
+	<!--BEGIN STORY LISTING-->
+		<div class="story">
+			<!--STORY TOP-->
+			<div class="story-top">
+				<div class="author">
+					<div class="author-image"></div>
+					<div class="byline">
+						<p>Timothy Johnson <span>2 hours ago</span></p>
+
+					</div>
+				</div>
+				<div class="recommend">
+					<div class="heart"></div>
+					<div class="count">23</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<!--STORY TOP-->
+			<!--END STORY BODY-->
+			<div class="story-body">
+				
+				<!--SCALED IMAGE-->
+				<div class="scale-image" style="background-image: url(images/default_story.jpg);"></div>
+				<!--END SCALED IMAGE-->				
+
+				<h4>Can the in-coming government fix Nigeria's economy?</h4>
+				<p>Nigeria’s 2015 presidential election may have been won and lost but while still savouring his victory at the poll.</p>
+				<a href="#" class="read-more">Read more</a>
+			</div>
+			<!--END STORY BODY-->
+			<!--STORY META-->
+				<div class="story-meta">
+					<div class="tags">
+						<p>Posted in:</p>
+						<ul>
+							<li><a href="#" class="tag">Life</a></li>
+							<li><a href="#" class="tag">Learn</a></li>
+						</ul>
+					</div>
+					<div class="read-length">11 min read</div>
+					<div class="clear"></div>
+				</div>
+			<!--END STORY META-->
+		</div>
+	<!--END STORY-->
+	<!--BEGIN STORY LISTING-->
+	<div class="story">
+		<!--STORY TOP-->
+		<div class="story-top">
+			<div class="author">
+				<div class="author-image"></div>
+				<div class="byline">
+					<p>Timothy Johnson <span>2 hours ago</span></p>
+
+				</div>
+			</div>
+			<div class="recommend">
+				<div class="heart"></div>
+				<div class="count">23</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<!--STORY TOP-->
+		<!--END STORY BODY-->
+		<div class="story-body">
+			<!--SCALED IMAGE-->
+			<div class="scale-image" style="background-image: url(images/story-feed-test-1.jpg);"></div>
+			<!--END SCALED IMAGE-->	
+			<h4>Can the in-coming government fix Nigeria's economy?</h4>
+			<p>Nigeria’s 2015 presidential election may have been won and lost but while still savouring his victory at the poll.</p>
+			<a href="#" class="read-more">Read more</a>
+		</div>
+		<!--END STORY BODY-->
+		<!--STORY META-->
+			<div class="story-meta">
+				<div class="tags">
+					<p>Posted in:</p>
+					<ul>
+						<li><a href="#" class="tag">Life</a></li>
+						<li><a href="#" class="tag">Learn</a></li>
+					</ul>
+				</div>
+				<div class="read-length">11 min read</div>
+				<div class="clear"></div>
+			</div>
+		<!--END STORY META-->
+	</div>
+	<!--END STORY-->
+		<!--BEGIN STORY LISTING-->
+	<div class="story">
+		<!--STORY TOP-->
+		<div class="story-top">
+			<div class="author">
+				<div class="author-image"></div>
+				<div class="byline">
+					<p>Timothy Johnson <span>2 hours ago</span></p>
+
+				</div>
+			</div>
+			<div class="recommend">
+				<div class="heart"></div>
+				<div class="count">23</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<!--STORY TOP-->
+		<!--END STORY BODY-->
+		<div class="story-body">
+			<!--SCALED IMAGE-->
+			<div class="scale-image" style="background-image: url(images/story-feed-test-2.jpg);"></div>
+			<!--END SCALED IMAGE-->	
+			<h4>Can the in-coming government fix Nigeria's economy?</h4>
+			<p>Nigeria’s 2015 presidential election may have been won and lost but while still savouring his victory at the poll.</p>
+			<a href="#" class="read-more">Read more</a>
+		</div>
+		<!--END STORY BODY-->
+		<!--STORY META-->
+			<div class="story-meta">
+				<div class="tags">
+					<p>Posted in:</p>
+					<ul>
+						<li><a href="#" class="tag">Life</a></li>
+						<li><a href="#" class="tag">Learn</a></li>
+					</ul>
+				</div>
+				<div class="read-length">11 min read</div>
+				<div class="clear"></div>
+			</div>
+		<!--END STORY META-->
+	</div>
+	<!--END STORY-->
+	<div class="load-more-button">
+		<div class="relative">
+			Load More
+			<div class="arrow"></div>
+		</div>
+	</div>
+
+</div>
