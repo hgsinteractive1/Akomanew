0f791c32a6a6fa00d4c8fc876b7388b4f3b3d3db
Ken Koch <kkoch986@gmail.com>
2015-06-07 11:09:30 -0400

    fix link funcitonality

--- ghost/core/client/public/assets/js/grande.js
+++ ghost/core/client/public/assets/js/grande.js
@@ -535,11 +535,15 @@
         target = e.target || e.srcElement;
 
     // The selected text is not editable
+    console.log()
     if (!target.isContentEditable || $(target).closest(".content").length === 0) {
-      reloadMenuState();
-      setTextMenuPosition(EDGE, EDGE);
-      textMenu.className = "text-menu hide";
-      return;
+      // Make sure not to close the menu while the user is interacting with the menu...
+      if($(target).closest(".text-menu.active").length === 0) {
+        reloadMenuState();
+        setTextMenuPosition(EDGE, EDGE);
+        textMenu.className = "text-menu hide";
+        return;
+      }
     }
 
     // The selected text is collapsed, push the menu out of the way
